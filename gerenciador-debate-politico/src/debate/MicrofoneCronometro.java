ddff575e60d1bccf620f8bb23c4132317cad5f1c
viniciusfigueiredoo <viniciusqalmeida96@gmail.com>
2026-06-10 02:06:36 -0300

    feat: adicionei botão DR ao MicrofoneCronometro com pressionarBotaoDR()

--- gerenciador-debate-politico/src/debate/MicrofoneCronometro.java
+++ gerenciador-debate-politico/src/debate/MicrofoneCronometro.java
@@ -1,18 +1,35 @@
 package debate;
 
+import debate.prototype.PoliticoConcrete;
+
 public class MicrofoneCronometro {
     private boolean microfoneAtivo;
+    private MediadorDebate mediador;
+    private PoliticoConcrete dono;
+
+    // Construtor
+    public MicrofoneCronometro(MediadorDebate mediador, PoliticoConcrete dono) {
+        this.mediador = mediador;
+        this.dono = dono;
+    }
 
     // Métodos auxiliares
     public void setMicrofoneAtivo(boolean microfoneAtivo) {
         this.microfoneAtivo = microfoneAtivo;
     }
+
     public void ativar(){
         setMicrofoneAtivo(true);
     }
+
     public void desativar(){
         setMicrofoneAtivo(false);
     }
+
+    public void pressionarBotaoDR(){
+        mediador.requisitarDR(dono);
+    }
+
     public void esperarTempo(int tempo){
         int tempoSeg = tempo * 1000;
         try {
