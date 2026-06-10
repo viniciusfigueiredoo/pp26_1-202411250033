18b81812dfd8bd7afc6a1aaac32f3659806a7676
viniciusfigueiredoo <viniciusqalmeida96@gmail.com>
2026-06-10 02:06:44 -0300

    feat: adicionei suporte ao tempo de DR no Builder

--- gerenciador-debate-politico/src/builder/ConfigProduct.java
+++ gerenciador-debate-politico/src/builder/ConfigProduct.java
@@ -5,16 +5,25 @@ public class ConfigProduct {
     private int resposta;
     private int replica;
     private int treplica;
+    private int dr;
 
+    // Construtor
     public ConfigProduct() {}
 
+    // Métodos auxiliares
     public void setPergunta(int tempo) { this.pergunta = tempo; }
     public void setResposta(int tempo) { this.resposta = tempo; }
     public void setReplica(int tempo) { this.replica = tempo; }
     public void setTreplica(int tempo) { this.treplica = tempo; }
+    public void setDr(int dr) {
+        this.dr = dr;
+    }
 
     public int getPergunta() { return pergunta; }
     public int getResposta() { return resposta; }
     public int getReplica() { return replica; }
     public int getTreplica() { return treplica; }
+    public int getDr() {
+        return dr;
+    }
 }
